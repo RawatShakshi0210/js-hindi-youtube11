8ed235bb437cd544335bec59086c27aad2e3f20e
RawatShakshi0210 <shakshirawat02@gmail.com>
2026-07-11 19:54:39 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 145367
+let accountEmail = "sahhsjahjj@gmail.com"
+var accountPassword = "12346788"
+accountCity = "jaipur"
+let accountState;
+
+
+
+//accountId =  2 // not allowed
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+
+accountEmail = "shgssu@.com"
+accountPassword = "236271872282"
+accountCity = "agra"
+
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,53 @@
+"use strict";//this data type treat all JS code as new version
+
+// alert (3 + 3) // we are using nodejs,not browser,in node js they creat error
+
+console.log(3
+    +3) //code readability should be high,not a good way to write a code
+
+console.log("shakshi")
+
+
+let name = "shakshi" 
+let age = 18
+let isLoggedIn = false
+
+// number=>2to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "shakshi"); // string,,
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// this line of code will help you to find the datatype of anything
