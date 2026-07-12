c2054284cd402b64aba26fb8ec7227a74f1aa85a
RawatShakshi0210 <shakshirawat02@gmail.com>
2026-07-12 11:45:04 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+/*let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+//"33" => 33;convert easily
+//"33abc" => convert in NaN , not a number
+// true => 1; false => 0
+
+/*let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);*/
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+
+
+
+
